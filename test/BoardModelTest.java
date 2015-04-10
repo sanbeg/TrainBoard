b27e1ff195381955dba55e21429adaea27f73f1b
Steve Sanbeg <steve@hpunr.(none)>
2015-04-10 09:34:41 -0400

    more tests

--- test/BoardModelTest.java
+++ test/BoardModelTest.java
@@ -21,14 +21,18 @@ class TestShape implements Shape
     public Color color;
     public int nDraw = 0;
     public int nErase = 0;
+    public GraphicsContext drawGc = null;
+    public GraphicsContext eraseGc = null;
     
     public void draw(GraphicsContext gc, Color color) {
         ++nDraw;
         this.color = color;
+	drawGc = gc;
     }
     
     public void erase(GraphicsContext gc){
         ++nErase;
+	eraseGc = gc;
     }
     
 }
@@ -38,6 +42,7 @@ public class BoardModelTest extends TestCase
 {
     final Canvas canvas = new Canvas(250,250);
     final GraphicsContext gc = canvas.getGraphicsContext2D();
+    final GraphicsContext fgc = new Canvas(250,250).getGraphicsContext2D();
 
     public void testAdd() {
         BoardModel bm = new BoardModel();
@@ -64,6 +69,26 @@ public class BoardModelTest extends TestCase
         assertEquals("Changed Color", ts.color, Color.GREEN);
     }
 
+    public void testLiftToFloat() {
+        BoardModel bm = new BoardModel();
+	bm.setFloatingContext(fgc);
+	
+        TestShape ts = new TestShape();
+        bm.addShape(gc, 100, 100, ts);
+        assertEquals("Called draw", 1, ts.nDraw);
+        bm.liftShape(gc, 100, 100);
+        assertEquals("Called draw again", 2, ts.nDraw);
+        assertEquals("Used Color", ts.color, Color.BLUE);
+	assertEquals("Erased on fixed", ts.eraseGc, gc);
+	assertEquals("Drew on float", ts.drawGc, fgc);
+	
+        bm.releaseShape(gc);
+	assertEquals("Erased on float", ts.eraseGc, fgc);
+        assertEquals("Called draw again", 3, ts.nDraw);
+        assertEquals("Changed Color", ts.color, Color.GREEN);
+	assertEquals("Drew on fixed", ts.drawGc, gc);
+    }
+
     public void testMove() {
         BoardModel bm = new BoardModel();
         TestShape ts = new TestShape();
@@ -79,6 +104,25 @@ public class BoardModelTest extends TestCase
         assertEquals("found at center", ts, bm.findPointAt(200, 100).shape);
         assertEquals("called erase", 2, ts.nErase);
     }
+
+    public void testMoveOnFloat() {
+        BoardModel bm = new BoardModel();
+	bm.setFloatingContext(fgc);
+	
+        TestShape ts = new TestShape();
+        bm.addShape(gc, 100, 100, ts);
+        bm.liftShape(gc, 100, 100);
+        assertEquals("called erase", 1, ts.nErase);
+
+        bm.moveShape(gc, 100, 0);
+
+        assertNull("old place is empty", bm.findPointAt(100,100));
+        assertNull("left is empty", bm.findPointAt(180,100));
+        assertNull("right is empty", bm.findPointAt(220,100));
+        assertEquals("found at center", ts, bm.findPointAt(200, 100).shape);
+        assertEquals("called erase", 2, ts.nErase);
+	assertEquals("Erased on float", fgc, ts.eraseGc);
+    }
     
     public void testRotate() {
         BoardModel bm = new BoardModel();
@@ -93,7 +137,6 @@ public class BoardModelTest extends TestCase
         assertEquals("found at center", ts, bm.findPointAt(100,100).shape);
         assertEquals("found left", ts, bm.findPointAt(80,100).shape);
         assertEquals("found right", ts, bm.findPointAt(120,100).shape);
-
     }
 
     public void testAddSnap() {
@@ -142,7 +185,6 @@ public class BoardModelTest extends TestCase
 	assertEquals("Angle matches", p1.angle, p2.angle, 0.01);
         assertEquals("Y aligned", p1.y, p2.y, 0.001);
 	assertEquals("X is snapped", p1.x+100, p2.x, 0.001);
-	
     }
     
 }
