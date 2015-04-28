69038e7e36a44719df9090c5eae0373b50ce2713
Steve Sanbeg <steve.sanbeg@gmail.com>
2015-04-27 22:48:00 -0400

    more tracks

--- ShapeBox.java
+++ ShapeBox.java
@@ -7,12 +7,17 @@ public class ShapeBox {
 	TrackScale nscale = TrackScale.N;
 	shapes.add(new Track.Straight("straight", nscale, new Length(2.5)));
 	shapes.add(new Track.Straight("straight5", nscale, new Length(5)));
-	shapes.add(new Track.Cross("x90", nscale, new Length(2.0), 90));
-	shapes.add(new Track.Cross("x45", nscale, new Length(2.0), 45));
-	shapes.add(new Track.Road("road", nscale, new Length(1.0), new Length(2.0)));
+
 	shapes.add(new Track.Curve("curve", nscale, new Length(9.5), 30.0));
 	shapes.add(new Track.Curve("half-curve", nscale, new Length(9.5), 15.0));
+	shapes.add(new Track.Curve("curve-11", nscale, new Length(11), 30.0));
 	shapes.add(new Track.Curve("curve-19", nscale, new Length(19), 15.0));
+
+	shapes.add(new Track.Cross("x90", nscale, new Length(2.0), 90));
+	shapes.add(new Track.Cross("x45", nscale, new Length(2.0), 45));
+	shapes.add(new Track.Cross("x15", nscale, new Length(5.0), 15));
+	shapes.add(new Track.Road("road", nscale, new Length(1.0), new Length(2.0)));
+
 	shapes.add(new Track.Turnout("right",
 				     nscale,
 				     Track.Turnout.Hand.RIGHT,
